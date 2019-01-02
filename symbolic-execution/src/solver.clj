b261ace8c066bfab59d31d1024c514f10c7a5e0d
Dominik Engelhardt <dominikengelhardt@ymail.com>
2019-01-02 18:01:34 +0100

    Improve performance

--- symbolic-execution/src/solver.clj
+++ symbolic-execution/src/solver.clj
@@ -1,13 +1,39 @@
 (ns solver
   (:require [clojure.java.shell :use sh]
             [clojure.edn :as edn]
-            [clojure.string :as s]))
+            [clojure.string :as s]
+            [clojure.walk :as walk]))
 
-(defn call-z3 [code]
+(defn- shell-z3 [code]
   (:out (sh "z3" "-smt2" "-in" :in code)))
 
-(defn reachable? [constraints]
+(defn- extract-symbols [constraints]
+  (set (filter #(:symbolic (meta %)) (flatten constraints))))
+  
+(defn- extract-sym-decls [constraints]
+  (->> constraints extract-symbols (map #(list 'declare-const % 'Int))))
+
+(defn call-z3 [constraints sym-decls]
   (let [assertions (map #(list 'assert %) constraints)
-        symbols (filter #(:symbolic (meta %)) (flatten constraints))
-        sym-decls (map #(list 'declare-const % 'Int) symbols)]
-    (s/starts-with? (call-z3 (str (s/join sym-decls) (s/join assertions) "(check-sat)")) "sat")))
+        commands (list '(check-sat))]
+    (shell-z3 (str (s/join (concat sym-decls assertions commands))))))
+
+(defn- z3-reachable? [z3-result]
+  (s/starts-with? z3-result "sat"))
+
+(defn reachable? [[valid :as constraints]]
+  (if (boolean? valid)
+    valid
+    (let [z3-result (call-z3 constraints (extract-sym-decls constraints))]
+      (when (s/starts-with? z3-result "(error") (throw (Exception. (str z3-result (vec (extract-sym-decls constraints)) constraints))))
+      (z3-reachable? z3-result))))
+
+(defn simplify [constraints]
+  (let [sym-decls (set (extract-sym-decls constraints))
+        symbols (extract-symbols constraints)
+        term (cons 'and constraints)
+        command (list 'simplify term)
+        code (s/join (concat sym-decls (list command)))
+        result-str (shell-z3 code)
+        result-code (edn/read-string result-str)]
+    (walk/prewalk #(if (symbols %) (with-meta % {:symbolic true}) %) result-code)))  
--- symbolic-execution/src/symbolic.clj
+++ symbolic-execution/src/symbolic.clj
@@ -1,18 +1,22 @@
 (ns symbolic
   (:require [solver]
             [mil-parser :as mil]))
-(defn bi-consumer [sym [pc mem [r l & stack] cs]] [[(inc pc) mem (vec (cons (list sym l r) stack)) cs]])
+
+(defrecord State [^int pc mem stack cs])
+
+(defn bi-consumer [sym {:keys [pc mem cs] [r l & stack] :stack}]
+  [(State. (inc pc) mem (vec (cons (list sym l r) stack)) cs)])
 (defn lookup [addr frame]
   (if (symbol? addr)
     (or (frame addr) 0)
     addr))
-;; state [pc mem stack constraints]
+
 (defmulti step (fn [inst state] (first inst)))
 
 (defmethod step 'add [_ state] (bi-consumer '+ state))
 (defmethod step 'sub [_ state] (bi-consumer '- state))
 (defmethod step 'mul [_ state] (bi-consumer '* state))
-(defmethod step 'div [_ state] (bi-consumer '/ state))
+(defmethod step 'div [_ state] (bi-consumer 'div state))
 
 (defmethod step 'eq [_ state] (bi-consumer '= state))
 (defmethod step 'neq [_ state] (bi-consumer 'distinct state))
@@ -21,35 +25,37 @@
 (defmethod step 'gt [_ state] (bi-consumer '> state))
 (defmethod step 'geq [_ state] (bi-consumer '>= state))
 
-(defmethod step 'neg [_ [pc mem [op & stack] cs]] [[(inc pc) mem (vec (cons (list 'not op) stack)) cs]])
-(defmethod step 'jpc [[_ addr] [pc mem [op & stack] cs]]
-  [[(inc pc) mem stack (vec (cons op cs))]
-   [addr mem stack (vec (cons (list 'not op) cs))]])
-(defmethod step 'yld [_ [pc mem stack cs]] [[(inc pc) mem stack cs]])
+(defmethod step 'neg [_ {:keys [pc mem cs] [op & stack] :stack}]
+  [(State. (inc pc) mem (vec (cons (list 'not op) stack)) cs)])
+(defmethod step 'jpc [[_ addr] {:keys [pc mem cs] [op & stack] :stack}]
+  [(State. (inc pc) mem stack [(solver/simplify (cons op cs))])
+   (State. addr mem stack [(solver/simplify (cons (list 'not op) cs))])])
+(defmethod step 'yld [_ state] [(update state :pc inc)])
 
-(defmethod step 'lod [[_ op] [pc [frame & frames :as mem] stack cs]]
-  [[(inc pc) mem (vec (cons (lookup op frame) stack)) cs]])
-(defmethod step 'sto [[_ op] [pc mem [v & stack] cs]]
-  [[(inc pc) (if op (assoc-in mem [0 op] v) mem) stack cs]])
+(defmethod step 'lod [[_ op] {:keys [pc stack cs] [frame & frames :as mem] :mem}]
+  [(State. (inc pc) mem (vec (cons (lookup op frame) stack)) cs)])
+(defmethod step 'sto [[_ op] {:keys [pc mem cs] [v & stack] :stack}]
+  [(State. (inc pc) (if op (assoc-in mem [0 op] v) mem) stack cs)])
 
-(defmethod step 'jmp [[_ addr] [pc mem stack cs]] [[addr mem stack cs]])
-(defmethod step 'cal [[_ addr] [pc mem stack cs]] [[addr (vec (cons {:ret (inc pc)} mem)) stack cs]])
-(defmethod step 'ret [_ [pc [frame & mem] stack cs]] [[(:ret frame) (vec mem) stack cs]])
-(defmethod step 'prt [_ [pc mem stack cs]] [[(inc pc) mem stack cs]])
-(defmethod step 'lbl [_ [pc mem stack cs]] [[(inc pc) mem stack cs]])
-(defmethod step 'inp [_ [pc mem stack cs]]
-  [[(inc pc) mem (vec (cons (with-meta (gensym) {:symbolic true}) stack)) cs]])
+(defmethod step 'jmp [[_ addr] state] [(assoc state :pc addr)])
+(defmethod step 'cal [[_ addr] {:keys [pc mem stack cs]}] [(State. addr (vec (cons {:ret (inc pc)} mem)) stack cs)])
+(defmethod step 'ret [_ {:keys [pc stack cs] [frame & mem] :mem}] [(State. (:ret frame) (vec mem) stack cs)])
+(defmethod step 'prt [_ state] [(update state :pc inc)])
+(defmethod step 'lbl [_ state] [(update state :pc inc)])
+(defmethod step 'inp [_ {:keys [pc mem stack cs]}]
+  [(State. (inc pc) mem (vec (cons (with-meta (gensym) {:symbolic true}) stack)) cs)])
 
-(defn run-sym [max-depth prog [pc _ _ cs :as state]]
-  (let [done? (and (>= pc (count prog)) (pos? max-depth))
-        reachable? (solver/reachable? cs)]
-    (if (or done? (not reachable?))
-      {:state state :children []}
-      (let [follow-states (step (prog pc) state)
-            children (mapv #(run-sym (dec max-depth) prog %) follow-states)]
-        {:state state :children children}))))
+(defn terminated? [prog {:keys [pc]}] (>= pc (count prog)))
+
+(defn sym-step [prog]
+  (fn [{:keys [pc cs] :as state}]
+    (let [inst (prog pc)
+          children (step inst state)
+          not-terminated (filter #(not (terminated? prog %)) children)
+          reachable (filter #(solver/reachable? (:cs %)) not-terminated)]
+      (mapv #(with-meta % {:instruction (prog (:pc %))}) reachable))))
 
 (defn verify [file]
   (let [program (mil/parse file)
-        tree (run-sym 100 program [0 [{}] [] []])]
-    tree))
+        states (tree-seq #(not (terminated? program %)) (sym-step program) (State. 0 [{}] [] []))]
+    (vec (take 1000 states))))
