dcb7f148292bdbfd10387633308e148491cbef7e
Dominik Engelhardt <dominikengelhardt@ymail.com>
2019-01-23 18:50:55 +0100

    Run verification on debug

--- edu.mdsd.mil.resource.mil/src/edu/mdsd/mil/resource/mil/launch/MilLaunchConfigurationDelegate.java
+++ edu.mdsd.mil.resource.mil/src/edu/mdsd/mil/resource/mil/launch/MilLaunchConfigurationDelegate.java
@@ -8,12 +8,22 @@ package edu.mdsd.mil.resource.mil.launch;
 
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
 
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.ui.console.ConsolePlugin;
 import org.eclipse.ui.console.IConsole;
 import org.eclipse.ui.console.IConsoleManager;
@@ -21,6 +31,7 @@ import org.eclipse.ui.console.IOConsole;
 
 import edu.mdsd.mil.MILModel;
 import edu.mdsd.mil.interpreter.MILInterpreter;
+import edu.mdsd.symbolic.Symbolic;
 
 /**
  * A class that handles launch configurations.
@@ -38,9 +49,49 @@ public class MilLaunchConfigurationDelegate extends LaunchConfigurationDelegate 
 		MILModel model = (MILModel) helper.getModelRoot(configuration);
 		IOConsole console = findConsole("Interpreter");
 		PrintStream out = new PrintStream(console.newOutputStream());
-		InputStream in = console.getInputStream();
-		new MILInterpreter(out, in).interpretAndPrintResult(model);
+
+		switch (mode) {
+		case "debug":
+			debug(helper, configuration, out);
+			break;
+		default: // run
+			InputStream in = console.getInputStream();
+			new MILInterpreter(out, in).interpretAndPrintResult(model);
+		}
+
 		out.close();
+		monitor.done();
+	}
+
+	private static void debug(MilLaunchConfigurationHelper helper, ILaunchConfiguration configuration,
+			PrintStream out) {
+		try {
+			URI uri = helper.getURI(configuration);
+			String file = new URL(uri.toString()).toString();
+			@SuppressWarnings("unchecked")
+			Map<Integer, List<String>> errors = Symbolic.verify(file);
+			IResource resource = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(uri.toPlatformString(true)));
+			resource.deleteMarkers(IMarker.PROBLEM, true, IResource.DEPTH_INFINITE);
+			errors.forEach(createMarkers(resource));
+			out.println(errors);
+		} catch (MalformedURLException | CoreException e) {// ignore
+		}
+	}
+
+	private static BiConsumer<Integer, List<String>> createMarkers(IResource resource) {
+		return (line, messages) -> {
+			messages.forEach(message -> {
+				IMarker m;
+				try {
+					m = resource.createMarker(IMarker.PROBLEM);
+					m.setAttribute(IMarker.LINE_NUMBER, line);
+					m.setAttribute(IMarker.MESSAGE, message);
+					m.setAttribute(IMarker.PRIORITY, IMarker.PRIORITY_HIGH);
+					m.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_ERROR);
+				} catch (CoreException e) {// ignore
+				}
+			});
+		};
 	}
 
 	private static IOConsole findConsole(String name) {
